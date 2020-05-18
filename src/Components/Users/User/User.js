fc0fc6bcbf3eeb77068d2929c9afefa6c19183f4
Stas <nevmyvakastas@gmail.com>
2020-05-18 22:27:02 +0600

    added post, delete request. (follow)

--- src/Components/Users/User/User.js
+++ src/Components/Users/User/User.js
@@ -2,8 +2,38 @@ import React from 'react'
 import s from './User.module.css'
 import userPhoto from './../../../Assets/userImg.png'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
 export const User = (props)=>{
+    const followTest = (userId) =>{
+        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {}, {
+            withCredentials: true,
+            headers: {
+                "API-KEY" : "6c351a34-68f0-4397-9304-ce329d07358e" 
+            }
+        })
+            .then(response => {
+                if( response.data.resultCode === 0 ){
+                    props.follow(userId)
+                }
+            })
+    }
+
+    const unFollowTest = (userId) =>{
+        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {
+            withCredentials: true,
+            headers: {
+                "API-KEY" : "6c351a34-68f0-4397-9304-ce329d07358e" 
+            }
+        })
+            .then(response => {
+                if( response.data.resultCode === 0 ){
+                    props.unFollow(userId)
+                }
+            })
+    }
+    
+
     return(
         <div className={s.userContainer}>
             <NavLink to={'/profile/' + props.user.id}>
@@ -21,9 +51,17 @@ export const User = (props)=>{
                     <div>{props.user.location.city}</div>
                 </div> */}
             </div>
+            
 
-            {props.user.followed ? <button className={s.unfollowed} onClick = {() => {props.unFollow(props.user.id)}}>Unfollow</button>
-                                 : <button className={s.followed} onClick={() => {props.follow(props.user.id)}}>Follow</button> }         
+            {props.user.followed ? <button className={s.unfollowed} onClick = {() => {
+                //props.unFollow(props.user.id)
+                unFollowTest(props.user.id)
+            
+            }}>Unfollow</button>
+                                 : <button className={s.followed} onClick={() => {
+            //props.follow(props.user.id)
+                followTest(props.user.id)
+            }}>Follow</button> }         
         </div>
     )
 }
