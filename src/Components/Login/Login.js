14ddc63e00ea6e75a15bbad034a43b1c0c5d52cf
Stas <nevmyvakastas@gmail.com>
2020-05-26 00:56:17 +0600

    added login form, styles

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,34 @@
 import React from 'react'
+import s from './Login.module.css'
 
-export const Login = () =>{
-    return <h>Login</h>
+
+export const Login = (props) => {
+    return(
+        <div className={s.loginFormContainer}>
+            <p className={s.heading}>Login</p>
+            <LoginForm />
+        </div>
+    )
 }
+
+export const LoginForm = () =>{
+    return (
+        <form >
+            <div>
+                <input className={s.inputStyle} placeholder={"Login"}/>
+            </div>
+            <div>
+                <input className={s.inputStyle} placeholder={"Password"}/>
+            </div>
+            <div>
+                <input className = {s.rememberMeCb} type={"checkbox"}/>Remember me
+            </div>
+            <div>
+                <button className={s.loginBtn}>Login</button>
+            </div>
+        </form>
+    )
+}
+
+
 
